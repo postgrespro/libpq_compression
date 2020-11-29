614b8ee406fda6f2868e542811d8c988e24fce73
Konstantin Knizhnik <knizhnik@garret.ru>
2020-11-29 14:57:18 +0300

    Rename COMPRESSION option to PGCOMPRESSION

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -351,7 +351,7 @@ static const internalPQconninfoOption PQconninfoOptions[] = {
 		"Replication", "D", 5,
 	offsetof(struct pg_conn, replication)},
 
-	{"compression", "COMPRESSION", NULL, NULL,
+	{"compression", "PGCOMPRESSION", NULL, NULL,
 		"Libpq-compression", "", 16,
 	offsetof(struct pg_conn, compression)},
 
